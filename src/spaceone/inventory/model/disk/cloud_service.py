c629a5ee1f54478dcf1c193ffa8c4c126aa5477e
Jihyung Song <bluese@megazone.com>
2020-12-04 20:56:46 +0900

    code review..

--- src/spaceone/inventory/model/disk/cloud_service.py
+++ src/spaceone/inventory/model/disk/cloud_service.py
@@ -2,31 +2,37 @@ from schematics.types import ModelType, StringType, PolyModelType
 
 from spaceone.inventory.model.disk.data import Disk
 from spaceone.inventory.libs.schema.metadata.dynamic_field import TextDyField, DateTimeDyField, EnumDyField, ListDyField
-from spaceone.inventory.libs.schema.metadata.dynamic_layout import ItemDynamicLayout, TableDynamicLayout, ListDynamicLayout
+from spaceone.inventory.libs.schema.metadata.dynamic_layout import ItemDynamicLayout, TableDynamicLayout, \
+    ListDynamicLayout
 from spaceone.inventory.libs.schema.cloud_service import CloudServiceResource, CloudServiceResponse, CloudServiceMeta
 
 '''
 DISK
 '''
 disk_info_meta = ItemDynamicLayout.set_fields('Disk', fields=[
-    # TODO: Fill out to Dynamic Item Field Tab
-    TextDyField.data_source('ID', 'data.id'),
     TextDyField.data_source('Name', 'data.name'),
+    TextDyField.data_source('Storage Account Type', 'data.sku.name'),
+    TextDyField.data_source('Size(GiB)', 'data.disk_size_gb'),
+    TextDyField.data_source('Attached VM', 'data.managed_by'),
+    TextDyField.data_source('Resource Group', 'data.resource_group'),
     TextDyField.data_source('Location', 'data.location'),
-    TextDyField.data_source('Attached VM', 'data.ManagedBy'),
-    TextDyField.data_source('Zone', 'data.zones'),
-    TextDyField.data_source('Name', 'data.skuTier'),
-
-    # TextDyField.data_source('Storage account type', 'data.type'),
-    TextDyField.data_source('Size(GiB)', 'data.diskSizeGB'),
-    TextDyField.data_source('State', 'data.diskState'),
-    TextDyField.data_source('Owner', 'data.managedBy'),
-    TextDyField.data_source('Resource Group', 'data.resourceGroup'),
-    TextDyField.data_source('Location', 'data.location'),
-    TextDyField.data_source('Type', 'data.type')
+    ListDyField.data_source('Zones', 'data.zones', options={
+        'delimiter': '<br>'
+    }),
+    TextDyField.data_source('Subscription', 'data.subscription_name'),
 ])
 
-disk_meta = CloudServiceMeta.set_layouts([disk_info_meta, ])
+# 예제일뿐..
+disk_info_lock = TableDynamicLayout.set_fields('Locks', 'data.locks', fields=[
+    TextDyField.data_source('Lock Name', 'name'),
+    TextDyField.data_source('Lock Type', 'type'),
+    TextDyField.data_source('Lock Scope', 'scope'),
+    ListDyField.data_source('Lock Notes', 'notes', options={
+        'delimiter': '<br>'
+    }),
+])
+
+disk_meta = CloudServiceMeta.set_layouts([disk_info_meta, disk_info_lock])
 
 
 class ComputeResource(CloudServiceResource):
