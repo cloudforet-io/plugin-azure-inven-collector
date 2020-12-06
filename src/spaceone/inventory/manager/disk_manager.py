828a980ba15650a5a1b5b8cdbda3ba8d7c0a83f2
jiyoon@mz.co.kr <jiyoon@mz.co.kr>
2020-12-07 00:28:28 +0900

    update disk manager
    update disk manager

--- src/spaceone/inventory/manager/disk_manager.py
+++ src/spaceone/inventory/manager/disk_manager.py
@@ -34,30 +34,45 @@ class DiskManager(AzureManager):
             # TODO: to be Implemented
             disk_dict = self.convert_dictionary(disk)
             sku_dict = self.convert_dictionary(disk.sku)
-
-
-
             creation_data_dict = self.convert_dictionary(disk.creation_data)
-            # 여기 요부분.. 함수로 뺄까
+
+            # update sku_dict
+            sku_dict.update({  # switch DiskStorageAccountType to disk_sku_name for user-friendly word.
+                # (ex.Premium SSD, Standard HDD..)
+                'name': self.get_disk_sku_name(sku_dict['name'])
+            })
+
+            # update creation_data dict
             if disk.creation_data.image_reference is not None:
                 image_reference_dict = self.convert_dictionary(disk.creation_data.image_reference)
                 creation_data_dict.update({
                     'image_reference': image_reference_dict
                 })
 
-            managed_by = disk_dict.get('managed_by')
+            # update disk_data dict
             disk_dict.update({
-                'resource_group': self.get_resource_group_from_id(disk_dict['id']),
+                'resource_group': self.get_resource_group_from_id(disk_dict['id']),  # parse resource group from ID
                 'subscription_id': secret_data['subscription_id'],
+                'subscription_name': '',
                 'size': disk_dict['disk_size_bytes'],
                 'sku': sku_dict,
-                'creation_data': creation_data_dict
+                'creation_data': creation_data_dict,
             })
-
+            managed_by = disk_dict.get('managed_by')  # get attached vm's name
             if managed_by is not None:
                 disk_dict.update({
                     'managedBy': self.get_attached_vm_name_from_managed_by(disk_dict['managed_by'])
                 })
+            tags = disk_dict.get('tags')  # update tags
+            if tags is not None:
+                disk_dict.update({
+                    'tags': self.get_tags(disk_dict)
+                })
+
+            # subscription dict
+            subscription_info_dict = {'subscription_id': disk_dict['subscription_id'],
+                                      'subscription_name': disk_dict['subscription_name']}
+            # self.set_subscription_info(disk_conn, subscription_info_dict)
 
             print("----disk_dict----")
             print(disk_dict)
@@ -70,11 +85,6 @@ class DiskManager(AzureManager):
                 'reference': ReferenceModel(disk_data.reference())
             })
 
-            '''
-            sku_resource = DiskResource({
-                'data': sku_data
-            })
-            '''
             # Must set_region_code method for region collection
             self.set_region_code(disk_data['location'])
 
@@ -90,3 +100,37 @@ class DiskManager(AzureManager):
     def get_attached_vm_name_from_managed_by(managed_by):
         attached_vm_name = managed_by.split('/')[8]
         return attached_vm_name
+
+    @staticmethod
+    def get_disk_sku_name(sku_tier):
+        if sku_tier == 'Premium_LRS':
+            sku_name = 'Premium SSD'
+        elif sku_tier == 'StandardSSD_LRS':
+            sku_name = 'Standard SSD'
+        elif sku_tier == 'Standard_LRS':
+            sku_name = 'Standard HDD'
+        else:
+            sku_name = 'Ultra SSD'
+        return sku_name
+
+    @staticmethod
+    def set_subscription_info(disk_conn, subscription_info_dict):  # 정제 된 애
+        # 0. subscription['id' 'name' 'tenant_id'] 배열 만들고
+        for subscription_info_dict in disk_conn.get_subscription_info():
+            subscription_info_slist = {}
+            # subscription_info_dict.update({
+            # subscription_info_dict['name']= subscription['id']
+            # })
+        # 2. API 날려서 return 받고 중복제거 한 new 배열에 넣고
+        # 3. id 키 기반으로 tenant 원래 subscription 에 매핑
+        return subscription_info_dict
+
+    @staticmethod
+    def get_tags(disk_dict):
+        tags = []
+        for k, v in disk_dict.get('tags', {}).items():
+            tags.append({
+                'key': k,
+                'value': v
+            })
+        return tags
