7838089c0c77daf03861fc036389961204d701f3
jiyoon@mz.co.kr <jiyoon@mz.co.kr>
2020-12-08 00:53:42 +0900

    update lock data model
    update lock data model

--- src/spaceone/inventory/model/disk/data.py
+++ src/spaceone/inventory/model/disk/data.py
@@ -26,12 +26,12 @@ class CreationData(Model):
 
 
 class SourceVault(Model):
-    id = StringType()
+    id = StringType(serialize_when_none=False)
 
 
 class DiskEncryptionKey(Model):
-    source_vault = ModelType(SourceVault)
-    secret_url = StringType()
+    source_vault = ModelType(SourceVault, serialize_when_none=False)
+    secret_url = StringType(serialize_when_none=False)
 
 
 class KeyEncryptionKey(Model):
@@ -40,19 +40,19 @@ class KeyEncryptionKey(Model):
 
 
 class EncryptionSettingsCollection(Model):
-    disk_encryption_key = ModelType(DiskEncryptionKey)
-    key_encryption_key = ModelType(KeyEncryptionKey)
+    disk_encryption_key = ModelType(DiskEncryptionKey, serialize_when_none=False)
+    key_encryption_key = ModelType(KeyEncryptionKey, serialize_when_none=False)
 
 
 class Encryption(Model):
-    disk_encryption_set_id = StringType(serialize_when_none=False)
+    disk_encryption_set_id = StringType(default='', serialize_when_none=False)
     type = StringType(choices=('EncryptionAtRestWithCustomerKey', 'EncryptionAtRestWithPlatformAndCustomerKeys',
                                'EncryptionAtRestWithPlatformKey'),
-                      default='EncryptionAtRestWithPlatformKey')
+                      default='EncryptionAtRestWithPlatformKey', serialize_when_none=False)
 
 
 class ShareInfoElement(Model):
-    vmUri = StringType(serialize_when_none=False)
+    vm_uri = StringType(serialize_when_none=False)
 
 
 class Tags(Model):
@@ -60,12 +60,25 @@ class Tags(Model):
     value = StringType()
 
 
-# 예제일뿐...
-class Lock(Model):
+class LockLevel(Model):
+    can_not_delete = StringType()
+    not_specified = StringType()
+    read_only = StringType()
+
+
+class ManagementLockObject(Model):
+    id = StringType()
     name = StringType()
+    level = ModelType(LockLevel)
+    level_display = StringType()
+    notes = StringType()
+    properties_owners = ListType(StringType())
     type = StringType()
-    scope = StringType()
-    notes = ListType(StringType())
+
+
+class Lock(Model):
+    next_link = StringType()
+    value = ModelType(ManagementLockObject)
 
 
 class Disk(Model):
@@ -84,8 +97,8 @@ class Disk(Model):
     disk_iops_read_only = BooleanType(serialize_when_none=False)
     disk_size_bytes = IntType()
     size = IntType()  # disk size for statistics
-    encryption_settings_collection = ModelType(EncryptionSettingsCollection, serialize_when_none=False)
-    encryption = ModelType(Encryption, serialize_when_none=False),
+    encryption_settings_collection = ModelType(EncryptionSettingsCollection)
+    encryption = ModelType(Encryption)
     hyper_v_generation = StringType(serialize_when_none=False)
     time_created = DateTimeType()
     creation_data = ModelType(CreationData)
@@ -96,16 +109,11 @@ class Disk(Model):
     disk_m_bps_read_write = IntType()
     subscription_id = StringType()
     subscription_name = StringType()
-    locks = ListType(ModelType(Lock))
+    # locks = ListType(ModelType(Lock), serialize_when_none=False)
     disk_m_bps_read_only = BooleanType(serialize_when_none=False)
     disk_state = StringType(choices=('ActiveSAS', 'ActiveUpload', 'Attached', 'ReadyToUpload', 'Reserved', 'Unattached'))
-    disk_state_display = StringType()  # TODO : display set
     network_access_policy = StringType(choices=('AllowAll', 'AllowPrivate', 'DenyAll'), serialize_when_none=False)
     network_access_policy_display = StringType()
-    tier = StringType(choices=('P1', 'P2', 'P3', 'P4', 'P6', 'P10', 'P15', 'P20',
-                               'P30', 'P40', 'P50', 'P60', 'P70', 'P80', 'E1', 'E2', 'E3', 'E4', 'E6', 'E10', 'E15',
-                               'E20', 'E30', 'E40', 'E50', 'S4', 'S6', 'S10', 'S15', 'S20', 'S30', 'S40', 'S50',
-                               'S60', 'S70', 'S80'), serialize_when_none=False)
     tier_display = StringType(default='')
     tags = ListType(ModelType(Tags), default=[])
 
