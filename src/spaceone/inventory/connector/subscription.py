849cd33dcd2efed32611cd0043b042bdf18b075d
Jihyung Song <bluese@megazone.com>
2020-12-08 11:43:24 +0900

    add subscription info

--- src/spaceone/inventory/connector/subscription.py
+++ src/spaceone/inventory/connector/subscription.py
@@ -3,18 +3,15 @@ import logging
 from spaceone.inventory.libs.connector import AzureConnector
 from spaceone.inventory.error import *
 
-__all__ = ['DiskConnector']
+__all__ = ['SubscriptionConnector']
 _LOGGER = logging.getLogger(__name__)
 
 
-class DiskConnector(AzureConnector):
+class SubscriptionConnector(AzureConnector):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.set_connect(kwargs.get('secret_data'))
 
-    def list_disks(self):
-        return self.compute_client.disks.list()
-
-    def get_lock_info(self, subscription):
-        return self.resource_client.subscriptions.list(subscription)
+    def get_subscription_info(self, subscription_id):
+        return self.subscription_client.subscriptions.get(subscription_id)
---
+++ src/spaceone/inventory/manager/subscription_manager.py
@@ -0,0 +1,25 @@
+from spaceone.inventory.libs.manager import AzureManager
+from spaceone.inventory.connector.subscription import SubscriptionConnector
+from spaceone.inventory.libs.schema.base import ReferenceModel
+from spaceone.inventory.model.disk import *
+from spaceone.inventory.model.disk.cloud_service import *
+from spaceone.inventory.connector.disk import DiskConnector
+from spaceone.inventory.model.disk.cloud_service_type import CLOUD_SERVICE_TYPES
+from datetime import datetime
+import time
+
+
+class SubscriptionManager(AzureManager):
+    connector_name = 'SubscriptionConnector'
+
+    def get_subscription_info(self, params):  # id 정제 된 애
+        secret_data = params['secret_data']
+        subscription_connector: SubscriptionConnector = self.locator.get_connector(self.connector_name,
+                                                                                   secret_data=secret_data)
+        subscription_info = subscription_connector.get_subscription_info(secret_data['subscription_id'])  # subscription_info = disk_conn.get_subscription_info(subscription)
+
+        return {
+            'subscription_id': subscription_info.subscription_id,
+            'subscription_name': subscription_info.display_name,
+            'tenant_id': subscription_info.tenant_id
+        }
