b716e8b71c2a2b09d9c92eaae2e873489b592520
jean1042 <jean1042@naver.com>
2020-12-11 18:18:42 +0900

    Change the order of snapshot metadata
    Change the order of snapshot metadata

--- src/spaceone/inventory/model/snapshot/cloud_service_type.py
+++ src/spaceone/inventory/model/snapshot/cloud_service_type.py
@@ -6,13 +6,10 @@ from spaceone.inventory.libs.schema.cloud_service_type import CloudServiceTypeRe
 
 cst_snapshot = CloudServiceTypeResource()
 cst_snapshot.name = 'Snapshot'
-cst_snapshot.provider = 'azure'
 cst_snapshot.group = 'Compute'
 cst_snapshot.labels = ['Compute', 'Storage']
-cst_snapshot.is_major = False
 cst_snapshot.tags = {
     'spaceone:icon': 'https://spaceone-custom-assets.s3.ap-northeast-2.amazonaws.com/console-assets/icons/cloud-services/azure/azure-disk-snapshot.svg',
-    'spaceone:display_name': 'Snapshots'
 }
 
 cst_snapshot._metadata = CloudServiceTypeMeta.set_meta(
@@ -20,11 +17,11 @@ cst_snapshot._metadata = CloudServiceTypeMeta.set_meta(
         TextDyField.data_source('Name', 'data.name'),
         TextDyField.data_source('Source disk', 'data.source_disk_name'),
         TextDyField.data_source('Snapshot type', 'data.incremental_display'),
-        DateTimeDyField.data_source('Time created', 'data.time_created'),
         TextDyField.data_source('Source disk size(GiB)', 'data.disk_size_gb'),
         TextDyField.data_source('Resource Group', 'data.resource_group'),
         TextDyField.data_source('Location', 'data.location'),
         TextDyField.data_source('Subscription', 'data.subscription_name'),
+        DateTimeDyField.data_source('Time created', 'data.time_created')
     ],
     search=[
         SearchField.set(name='ID', key='data.id', data_type='string'),
@@ -39,7 +36,7 @@ cst_snapshot._metadata = CloudServiceTypeMeta.set_meta(
         SearchField.set(name='Encryption', key='data.encryption.type_display', data_type='string'),
         SearchField.set(name='Network Access Policy', key='data.network_access_policy', data_type='string'),
         SearchField.set(name='Provisioning State', key='data.provisioning_state', data_type='string'),
-        SearchField.set(name='Creation Time', key='data.time_created', data_type='datetime'),
+        SearchField.set(name='Creation Time', key='data.time_created', data_type='datetime')
     ]
 
 )
