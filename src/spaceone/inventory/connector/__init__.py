3b8e37f7d4e4aacedc9497f13990cc01ba30e81c
jean1042 <jean1042@naver.com>
2020-12-11 17:19:21 +0900

    Add subscription, snapshot connector
    Add subscription, snapshot connector

--- src/spaceone/inventory/connector/__init__.py
+++ src/spaceone/inventory/connector/__init__.py
@@ -1,2 +1,3 @@
 from spaceone.inventory.connector.disk import DiskConnector
 from spaceone.inventory.connector.subscription import SubscriptionConnector
+from spaceone.inventory.connector.snapshot import SnapshotConnector
