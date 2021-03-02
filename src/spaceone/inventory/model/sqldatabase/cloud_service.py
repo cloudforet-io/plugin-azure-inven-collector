3b50b986478e91df2c0e1bb69e97f3fda57efadf
jean1042 <jean1042@naver.com>
2021-03-02 11:34:03 +0900

    update cloud service meta data

--- src/spaceone/inventory/model/sqldatabase/cloud_service.py
+++ src/spaceone/inventory/model/sqldatabase/cloud_service.py
@@ -1,6 +1,7 @@
 from schematics.types import ModelType, StringType, PolyModelType
 
 from spaceone.inventory.model.sqldatabase.data import SqlDatabase
+from spaceone.inventory.model.sqlserver.data import SqlServer
 from spaceone.inventory.libs.schema.metadata.dynamic_field import TextDyField, DateTimeDyField, EnumDyField, \
     ListDyField
 from spaceone.inventory.libs.schema.metadata.dynamic_layout import ItemDynamicLayout, TableDynamicLayout, \
@@ -16,25 +17,49 @@ SQL DATABASES
 # Load balancing rule, NAT Rules, Public IP Addresses, Load Balancing Type
 sql_databases_info_meta = ItemDynamicLayout.set_fields('SQL Databases', fields=[
     TextDyField.data_source('Name', 'data.name'),
-    TextDyField.data_source('Resource Group', 'data.resource_group'),
+    TextDyField.data_source('Status', 'data.status'),
     TextDyField.data_source('Resource ID', 'data.id'),
+    TextDyField.data_source('Resource Group', 'data.resource_group'),
     TextDyField.data_source('Location', 'data.location'),
-    TextDyField.data_source('Subscription', 'data.subscription_id'),
-    TextDyField.data_source('SKU', 'data.sku.name'),
-    TextDyField.data_source('Backend pools', 'data.backend_address_pools_count_display'),
-    ListDyField.data_source('Health Probe', 'data.probes_display', options={
-        'delimiter': '<br>'
-    }),
-    ListDyField.data_source('Load Balancing Rule', 'data.load_balancing_rules_display', options={
-        'delimiter': '<br>'
-    }),
-    ListDyField.data_source('NAT Rules', 'data.inbound_nat_rules_display', options={
-        'delimiter': '<br>'
-    }),
-    ListDyField.data_source('Private IP Address', 'data.private_ip_address_display', options={
-        'delimiter': '<br>'
-    }),
+    TextDyField.data_source('Subscription ID', 'data.subscription_id'),
+    TextDyField.data_source('Server Name', 'data.sku.name'),
+    TextDyField.data_source('Elastic Pool', 'data.elastic_pool_id'),
+    TextDyField.data_source('Pricing Tier', 'data.pricing_tier_display'),
+    DateTimeDyField.data_source('Earliest Restore Point', 'data.earliest_restore_date'),
+    TextDyField.data_source('Collation', 'data.collation'),
+    DateTimeDyField.data_source('Creation Date', 'data.creation_date'),
+    TextDyField.data_source('Server Admin Login', 'data.administrator_login'),
+
+
 ])
+
+# TAB - Configure
+sql_databases_configure = ItemDynamicLayout.set_fields('Configure', fields=[
+    TextDyField.data_source('Pricing Tier', 'data.sku.tier'),
+    TextDyField.data_source('Compute Tier', 'data.compute_tier'),
+    TextDyField.data_source('Compute Hardware', 'data.sku.name'),
+    TextDyField.data_source('Licence Type', 'data.license_type'),
+    TextDyField.data_source('vCores', 'data.current_sku.capacity'),
+    TextDyField.data_source('Data max size', 'data.max_size_gb'),
+])
+
+# TAB - Geo-Replication TODO : create_mode : None (if create_mode is secondary : Find Region and list all..)
+# TAB - Connection Strings TODO : No API
+# TAB - Sync To Other Databases
+sql_databases_sync_group = ItemDynamicLayout.set_fields('Sync Group', 'data.sync_group', fields=[
+    TextDyField.data_source('Name', 'name'),
+    TextDyField.data_source('Status', 'sync_state')
+])
+sql_databases_sync_agent = ItemDynamicLayout.set_fields('Sync Agent', fields=[
+    TextDyField.data_source('Name', ''),
+    TextDyField.data_source('Status', '')
+])
+sql_databases_sync = CloudServiceMeta.set_layouts(
+    [sql_databases_sync_group, sql_databases_sync_agent])
+
+# TAB - Connection Strings TODO : Server-Level.. Skip?
+
+# TAB - 
 
 # TAB - tags
 sql_databases_info_tags = TableDynamicLayout.set_fields('Tags', 'data.tags', fields=[
