ef773dcf49effcf179c5af02d1e22d2c4e4afae9
jean1042 <jean1042@naver.com>
2021-03-05 18:01:23 +0900

    Update SqlServer state to enumfield metatype

--- src/spaceone/inventory/model/sqldatabase/cloud_service.py
+++ src/spaceone/inventory/model/sqldatabase/cloud_service.py
@@ -1,6 +1,6 @@
 from schematics.types import ModelType, StringType, PolyModelType
 
-from spaceone.inventory.model.sqldatabase.data import SqlDatabase
+from spaceone.inventory.model.sqldatabase.data import Database
 from spaceone.inventory.model.sqlserver.data import SqlServer
 from spaceone.inventory.libs.schema.metadata.dynamic_field import TextDyField, DateTimeDyField, EnumDyField, \
     ListDyField
@@ -16,8 +16,15 @@ SQL DATABASES
 # Resource Group, Location, Subscription, Subscription ID, SKU, Backend pool, Health probe,
 # Load balancing rule, NAT Rules, Public IP Addresses, Load Balancing Type
 sql_databases_info_meta = ItemDynamicLayout.set_fields('SQL Databases', fields=[
-    TextDyField.data_source('Name', 'data.name'),
-    TextDyField.data_source('Status', 'data.status'),
+    TextDyField.data_source('Database Name', 'data.name'),
+    EnumDyField.data_source('Status', 'status', default_state={
+        'safe': ['Online', 'Creating', 'Copying', 'Creating', 'OnlineChangingDwPerformanceTiers', 'Restoring',
+                 'Resuming', 'Scaling', 'Standby'],
+        'warning': ['AutoClosed', 'Inaccessible', 'Offline', 'OfflineChangingDwPerformanceTiers', 'OfflineSecondary',
+                    'Pausing', 'Recovering', 'RecoveryPending', 'Suspect'],
+        'disable': ['Disabled', 'Paused', 'Shutdown'],
+        'alert': ['EmergencyMode']
+    }),
     TextDyField.data_source('Resource ID', 'data.id'),
     TextDyField.data_source('Resource Group', 'data.resource_group'),
     TextDyField.data_source('Location', 'data.location'),
@@ -30,36 +37,31 @@ sql_databases_info_meta = ItemDynamicLayout.set_fields('SQL Databases', fields=[
     DateTimeDyField.data_source('Creation Date', 'data.creation_date'),
     TextDyField.data_source('Server Admin Login', 'data.administrator_login'),
 
-
 ])
 
 # TAB - Configure
 sql_databases_configure = ItemDynamicLayout.set_fields('Configure', fields=[
-    TextDyField.data_source('Pricing Tier', 'data.sku.tier'),
+    TextDyField.data_source('Service Tier', 'service_tier_display'),
     TextDyField.data_source('Compute Tier', 'data.compute_tier'),
-    TextDyField.data_source('Compute Hardware', 'data.sku.name'),
+    TextDyField.data_source('Compute Hardware', 'data.sku.family'),
     TextDyField.data_source('Licence Type', 'data.license_type'),
     TextDyField.data_source('vCores', 'data.current_sku.capacity'),
     TextDyField.data_source('Data max size', 'data.max_size_gb'),
+    TextDyField.data_source('Zone Redundant', 'data.zone_redundant'),
+    ListDyField.data_source('Sync Groups', 'data.sync_group_display'),
+    ListDyField.data_source('Sync Agents', 'data.sync_agent_display'),
+    TextDyField.data_source('Collation', 'data.collation'),
+    DateTimeDyField.data_source('Creation Date', 'data.creation_date')
 ])
 
-# TAB - Geo-Replication TODO : create_mode : None (if create_mode is secondary : Find Region and list all..)
-# TAB - Connection Strings TODO : No API
-# TAB - Sync To Other Databases
-sql_databases_sync_group = ItemDynamicLayout.set_fields('Sync Group', 'data.sync_group', fields=[
+# TAB - Diagnostic Settings
+sql_databases_diagnostic_settings = SimpleTableDynamicLayout.set_fields('Diagnostic Settings', 'data.diagnostic_settings', fields=[
     TextDyField.data_source('Name', 'name'),
-    TextDyField.data_source('Status', 'sync_state')
+    TextDyField.data_source('Storage Account', 'storage_account_id'),
+    TextDyField.data_source('Event Hub', 'event_hub_name'),
+    TextDyField.data_source('Log Analytics Workspace', 'workspace_id'),
 ])
-sql_databases_sync_agent = ItemDynamicLayout.set_fields('Sync Agent', fields=[
-    TextDyField.data_source('Name', ''),
-    TextDyField.data_source('Status', '')
-])
-sql_databases_sync = CloudServiceMeta.set_layouts(
-    [sql_databases_sync_group, sql_databases_sync_agent])
 
-# TAB - Connection Strings TODO : Server-Level.. Skip?
-
-# TAB - 
 
 # TAB - tags
 sql_databases_info_tags = TableDynamicLayout.set_fields('Tags', 'data.tags', fields=[
@@ -77,7 +79,7 @@ class DatabaseResource(CloudServiceResource):
 
 class SqlDatabaseResource(DatabaseResource):
     cloud_service_type = StringType(default='Sqldatabases')
-    data = ModelType(SqlDatabase)
+    data = ModelType(Database)
     _metadata = ModelType(CloudServiceMeta, default=sql_databases_meta, serialized_name='metadata')
 
 
