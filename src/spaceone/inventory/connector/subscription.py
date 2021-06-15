48ba733ac6f3de321c78f6fccec1c42fa1946717
jean1042 <jean1042@naver.com>
2021-06-15 18:54:27 +0900

    change init params
    change init params

--- src/spaceone/inventory/connector/subscription.py
+++ src/spaceone/inventory/connector/subscription.py
@@ -10,7 +10,8 @@ _LOGGER = logging.getLogger(__name__)
 class SubscriptionConnector(AzureConnector):
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super().__init__()
+
         self.set_connect(kwargs.get('secret_data'))
 
     def get_subscription_info(self, subscription_id):
