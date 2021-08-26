246de0a0e33e2df0d3470660a772b983820a21ae
jean1042 <jean1042@naver.com>
2021-08-26 15:07:27 +0900

    refactor: add error codes

--- src/spaceone/inventory/error/custom.py
+++ src/spaceone/inventory/error/custom.py
@@ -14,3 +14,23 @@ class ERROR_DRIVER(ERROR_BASE):
 class ERROR_NOT_INITIALIZED_EXCEPTION(ERROR_BASE):
     status_code = 'INTERNAL'
     message = 'Collector is not initialized. Please call initialize() method before using it.'
+
+
+class ERROR_CONNECTOR_INITIALIZE(ERROR_BASE):
+    status_code = 'INTERNAL'
+    message = 'Connector is failed to initialized. Connector = {e}.'
+
+
+class ERROR_CONNECTOR(ERROR_BASE):
+    status_code = 'INTERNAL'
+    message = 'Connector is failed to connect. Connector = {e}.'
+
+
+class ERROR_KEY_VAULTS(ERROR_BASE):
+    status_code = 'INTERNAL'
+    message = 'KeyVault manager is failed to get sub resources. Manager = {e}.'
+
+
+class ERROR_KEY_VAULTS_PERMISSION(ERROR_BASE):
+    status_code = 'INTERNAL'
+    message = 'KeyVault secret and certification information is failed. Please check the permission.'
