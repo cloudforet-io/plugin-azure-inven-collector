a7dfdb1f397fb6dd614d966f728417a750b81466
MINHO KIM <mino@mz.co.kr>
2022-09-25 21:01:05 +0900

    fix: fix list_database_by_server param name

--- src/spaceone/inventory/connector/sql_servers/connector.py
+++ src/spaceone/inventory/connector/sql_servers/connector.py
@@ -15,9 +15,6 @@ class SQLServersConnector(AzureConnector):
     def list_servers(self):
         return self.sql_client.servers.list()
 
-    def list_databases(self, resource_group, server_name):
-        return self.sql_client.databases.list_by_server(resource_group_name=resource_group, server_name=server_name)
-
     def list_server_azure_ad_administrators(self, resource_group, server_name):
         return self.sql_client.server_azure_ad_administrators.list_by_server(resource_group, server_name)
 
@@ -33,8 +30,8 @@ class SQLServersConnector(AzureConnector):
     def list_encryption_protectors(self, resource_group, server_name):
         return self.sql_client.encryption_protectors.list_by_server(resource_group_name=resource_group, server_name=server_name)
 
-    def list_databases_by_server(self, resource_group, server_name):
-        return self.sql_client.databases.list_by_server(resource_group, server_name)
+    def list_databases_by_server(self, resource_group_name, server_name):
+        return self.sql_client.databases.list_by_server(resource_group_name=resource_group_name, server_name=server_name)
 
     def list_elastic_pools_by_server(self, resource_group, server_name):
         return self.sql_client.elastic_pools.list_by_server(resource_group, server_name)
