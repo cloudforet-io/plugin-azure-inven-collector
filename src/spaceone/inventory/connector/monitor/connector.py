1cf07480f5fb1d5f095852ab80bbdf821f7d89c9
Minho Kim <mino@megazone.com>
2022-12-09 00:27:14 +0900

    feat: add blob count and size info for StorageAccounts
    Signed-off-by: Minho Kim <mino@megazone.com>

--- src/spaceone/inventory/connector/monitor/connector.py
+++ src/spaceone/inventory/connector/monitor/connector.py
@@ -15,3 +15,9 @@ class MonitorConnector(AzureConnector):
 
     def list_diagnostic_settings(self, resource_uri):
         return self.monitor_client.diagnostic_settings.list(resource_uri=resource_uri)
+
+    def list_metrics(self, resource_uri, metricnames, aggregation, timespan=None, interval=None):
+        return self.monitor_client.metrics.list(resource_uri, metricnames=metricnames, aggregation=aggregation,
+                                                timespan=timespan, interval=interval)
+
+
